4d1a01da29e6363affd358e2c273acfddca53ed7
Paul Fischer <me@paulmfischer.dev>
2022-04-29 15:13:09 -0500

    add ball velocity and collision detection with collider

--- src/game/ball.rs
+++ src/game/ball.rs
@@ -1,11 +1,12 @@
 use bevy::{
     math::{const_vec2, const_vec3},
     prelude::*,
+    sprite::collide_aabb::{collide, Collision},
 };
 
 use crate::menu_state::GameState;
 
-use super::prelude::GameEntity;
+use super::{prelude::GameEntity, Collider};
 
 // We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
 const BALL_STARTING_POSITION: Vec3 = const_vec3!([0.0, -50.0, 1.0]);
@@ -24,7 +25,11 @@ pub struct BallPlugin;
 
 impl Plugin for BallPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_set(SystemSet::on_enter(GameState::InGame).with_system(render_ball));
+        app.add_system_set(SystemSet::on_enter(GameState::InGame).with_system(render_ball))
+            .add_system_set(
+                SystemSet::on_update(GameState::InGame)
+                    .with_system(apply_velocity.before(check_for_collisions)),
+            );
     }
 }
 
@@ -47,4 +52,64 @@ fn render_ball(mut commands: Commands) {
             ..default()
         })
         .insert(Velocity(INITIAL_BALL_DIRECTION.normalize() * BALL_SPEED));
+}
+
+fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>) {
+    for (mut transform, velocity) in query.iter_mut() {
+        transform.translation.x += velocity.x; // * TIME_STEP;
+        transform.translation.y += velocity.y; // * TIME_STEP;
+    }
+}
+
+fn check_for_collisions(
+    mut commands: Commands,
+    // mut scoreboard: ResMut<Scoreboard>,
+    mut ball_query: Query<(&mut Velocity, &Transform), With<Ball>>,
+    collider_query: Query<(Entity, &Transform), With<Collider>>,
+    // mut collision_events: EventWriter<CollisionEvent>,
+) {
+    let (mut ball_velocity, ball_transform) = ball_query.single_mut();
+    let ball_size = ball_transform.scale.truncate();
+
+    // check collision with walls
+    for (collider_entity, transform) in collider_query.iter() {
+        let collision = collide(
+            ball_transform.translation,
+            ball_size,
+            transform.translation,
+            transform.scale.truncate(),
+        );
+
+        if let Some(collision) = collision {
+            // Bricks should be despawned and increment the scoreboard on collision
+            // if maybe_brick.is_some() {
+            //     scoreboard.score += 1;
+            //     commands.entity(collider_entity).despawn();
+            // }
+
+            // reflect the ball when it collides
+            let mut reflect_x = false;
+            let mut reflect_y = false;
+
+            // only reflect if the ball's velocity is going in the opposite direction of the
+            // collision
+            match collision {
+                Collision::Left => reflect_x = ball_velocity.x > 0.0,
+                Collision::Right => reflect_x = ball_velocity.x < 0.0,
+                Collision::Top => reflect_y = ball_velocity.y < 0.0,
+                Collision::Bottom => reflect_y = ball_velocity.y > 0.0,
+                Collision::Inside => { /* do nothing */ }
+            }
+
+            // reflect velocity on the x-axis if we hit something on the x-axis
+            if reflect_x {
+                ball_velocity.x = -ball_velocity.x;
+            }
+
+            // reflect velocity on the y-axis if we hit something on the y-axis
+            if reflect_y {
+                ball_velocity.y = -ball_velocity.y;
+            }
+        }
+    }
 }
--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,7 +1,12 @@
+use bevy::prelude::Component;
+
 mod ball;
 mod game_state;
 mod paddle;
 mod walls;
+
+#[derive(Component)]
+struct Collider;
 
 pub mod prelude {
     pub use crate::game::game_state::*;
