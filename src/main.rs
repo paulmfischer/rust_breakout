a9c68d7e79b951e3e2cd4cfc081caf7c039f2ada
Paul Fischer <me@paulmfischer.dev>
2022-05-06 19:15:53 -0500

    convert state changes to event system

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use crate::game::prelude::GamePlugin;
-use bevy::{prelude::*, window::WindowMode};
+use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::WindowMode,
+};
 use menu_state::*;
+use state_plugin::*;
 
 mod game;
 mod menu_state;
+mod state_plugin;
 mod utilities;
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Copy)]
 pub enum GameState {
     MainMenu,
     InGame,
     Paused,
     GameOver,
+    Exit,
 }
 
 const BACKGROUND_COLOR: Color = Color::rgb(0.20, 0.20, 0.20);
@@ -21,14 +28,17 @@ fn main() {
             width: 800.0,
             height: 600.0,
             title: "Breakout!!".to_string(),
-            // vsync: false,
             mode: WindowMode::Windowed,
             resizable: false,
             ..Default::default()
         })
+        .add_event::<StateChange>()
         .add_plugins(DefaultPlugins)
         .add_plugin(MenuPlugin)
         .add_plugin(GamePlugin)
+        .add_plugin(StatePlugin)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_state(GameState::MainMenu)
         .run();
